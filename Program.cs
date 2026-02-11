60498bc550f38870f60bcb111a18e3a80dae2649
Alisson Almeida Q. <antonio.queiroz@docente.senai.br>
2026-02-11 19:19:15 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Nível 1: Variáveis e Operações Básicas
+#region 1. Calculadora de média
+
+#endregion
+
+#region 2. Conversor de Idade
+
+#endregion
